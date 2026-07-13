b828614b676a1e30d464429defae5054833a1121
k-soliz <k.soliz@moyca.eu>
2026-07-13 22:21:41 +0200

    agregar clases para manejar listas de compra y pagadores: definir modelos de datos para la gestión de compras

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlmodel import SQLModel, Field
 
 
@@ -20,5 +22,17 @@ class ItemListacompra(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     producto_id: int | None = Field(default=None, foreign_key="producto.id")
     cantidad: int
-    comprado: bool = Field(default=False)
+    lista_compra_id: int | None = Field(default=None, foreign_key="listacompra.id")
+
+
+class ListaCompra(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    fecha: date
+    total: float
+    persona_id: int | None = Field(default=None, foreign_key="persona.id")
+
+
+class ListaCompraPagador(SQLModel, table=True):
+    lista_compra_id: int = Field(foreign_key="listacompra.id", primary_key=True)
+    persona_id: int = Field(foreign_key="persona.id", primary_key=True)
 
